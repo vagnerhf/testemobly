5bc52667da6c7f748a441f929e7ae9af81f73d02
Vagner Henrique Ferreira <vagner.henrique.ferreira@gmail.com>
2018-03-23 15:01:22 -0300

    Feita a migrations

--- src/Entity/Produto.php
+++ src/Entity/Produto.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\MinhaEntidade;
+use App\Repository\ProdutoRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
