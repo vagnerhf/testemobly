59c9faac8e76d2b64da4e0f22d800f7c1d4e7323
Vagner Henrique Ferreira <vagner.henrique.ferreira@gmail.com>
2018-03-26 11:58:56 -0300

    Atualização do .gitignore para excluir arquivos da IDE

---
+++ src/Migrations/Version20180324202638.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180324202638 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE pedido CHANGE complemento complemento LONGTEXT DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE pedido CHANGE complemento complemento LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
